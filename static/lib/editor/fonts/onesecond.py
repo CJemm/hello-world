fd9b99c82cda1f25d495e14e8f7db031e63fa183
CJemm <39981479+CJemm@users.noreply.github.com>
2018-07-09 11:33:59 +0800

    Create onesecond.py

---
+++ static/lib/editor/fonts/onesecond.py
@@ -0,0 +1,6 @@
+def log(*args, **kwargs):
+    format = 'Y%/m%/d% H%:M%:S%'
+    value = time.localtime(int(time.time()))
+    dt = time.strftime(format, value)
+    with open('cj.log.txt', 'a', encoding='utf-8') as f:
+        print(dt, *args, file=f, **kwargs)
