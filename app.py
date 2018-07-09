b0ee22f700bd24eddec640c9e6350784b7dd1775
CJemm <39981479+CJemm@users.noreply.github.com>
2018-07-09 10:33:06 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+from flask import Flask
+
+import config
+
+
+app = Flask(__name__)
+app.secret_key = config.secret_key
+
+
+from routes.index import main as index_routes
+from routes.topic import main as topic_routes
+from routes.reply import main as reply_routes
+from routes.board import main as board_routes
+from routes.mail import main as mail_routes
+app.register_blueprint(index_routes)
+app.register_blueprint(topic_routes, url_prefix='/topic')
+app.register_blueprint(reply_routes, url_prefix='/reply')
+app.register_blueprint(board_routes, url_prefix='/board')
+app.register_blueprint(mail_routes, url_prefix='/mail')
+
+
+if __name__ == '__main__':
+    config = dict(
+        host='0.0.0.0',
+        port=2000,
+        debug=True,
+    )
+    app.run(**config)
+
